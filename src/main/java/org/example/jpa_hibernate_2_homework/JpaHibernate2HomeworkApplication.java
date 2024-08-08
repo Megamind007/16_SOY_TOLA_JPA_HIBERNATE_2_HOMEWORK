b510d44a7f9d08c61829bd71863cad4ade3d3e9e
Megamind <soy.tola9999@gmail.com>
2024-08-08 15:49:43 +0700

    ...

---
+++ src/main/java/org/example/jpa_hibernate_2_homework/JpaHibernate2HomeworkApplication.java
@@ -0,0 +1,13 @@
+package org.example.jpa_hibernate_2_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernate2HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernate2HomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jpa_hibernate_2_homework/JpaHibernate2HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpa_hibernate_2_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernate2HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
